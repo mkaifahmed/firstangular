e5d72fe0a0ed9ca48e971f469242123eab34ab58
Moulvi Kaif Ahmed <mka1904@outlook.com>
2022-04-08 11:21:42 +0530

    first commit

---
+++ src/app/name-display/name-display.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-display',
+  templateUrl: './name-display.component.html',
+  styleUrls: ['./name-display.component.css']
+})
+export class NameDisplayComponent implements OnInit {
+  firstName: string = 'Kaif';
+  lastName: string = 'Ahmed';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
